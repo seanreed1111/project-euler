d318bf61831cb89778bbe61df40dbc5c9094840f
seanreed1111 <seanreed1111@gmail.com>
2019-05-17 12:26:54 -0400

    begins problem 003

--- 1-10/003_largest_prime_factor.py
+++ 1-10/003_largest_prime_factor.py
@@ -3,3 +3,14 @@
 The prime factors of 13195 are 5, 7, 13 and 29.
 What is the largest prime factor of the number 600851475143?
 '''
+#target = 600851475143
+target = 13195
+
+def prime(n): # int :=> bool
+  pass
+
+def find_prime_factors(n): # int :=> [int]
+  pass
+
+factors = find_prime_factors(target)
+answer = max(factors)
