9e051628719ed49a761199707a01d13ffd4dfd09
seanreed1111 <seanreed1111@gmail.com>
2019-05-17 13:43:50 -0400

    continues factorization algo

--- 1-10/003_largest_prime_factor.py
+++ 1-10/003_largest_prime_factor.py
@@ -16,3 +16,7 @@ def prime(n): # int :=> bool
   for i in range(2, int(sqrt(n)) + 1):
     if n % i == 0: return False
   return True
+
+def find_prime_factors(n): # int :=> [int]
+
+  pass
--- lib/find_prime_factors.py
+++ lib/find_prime_factors.py
@@ -1,45 +1,52 @@
 numbers = [6,17,18,104]
-#Question:
-#for any number n, how do you find the set of factors of n?
-#make a dict of prime numbers. Key and value
-#make a set of prime numbers and check membership in the set.
-
-# if number is even: #1
-#   divide by two,
-#   add two to set of factors
-#   save the dividend
-#   if dividend is even:
-#      goto 1
-#   if dividend is odd:
-  #    goto 2
-# else:
-  #   goto 2
-
-#if number is odd: #2
-#  divide by all primes less than the number
-#   if none divide evenly then you are done.
-# recusive?
-
-
-
-
-
-
-
 def prime(n): # int :=> bool
   if n == 2: return True
   for i in range(2, int(sqrt(n)) + 1):
     if n % i == 0: return False
   return True
 
+#Question:
+#for any number n, how do you find the set of factors of n?
+#make a dict of prime numbers. Key and value
+# key is the number, value is the exponent, ie how many times the number divides evenly, e.g. prime factors of 40 = {2:3, 5:1}
+#make a set of prime numbers and check membership in the set.
+
+# if number is even: #1
+#   divide by two,
+#   increment two as a factor
+#   save the dividend. This is the new number
+#   if dividend is 1 then you are DONE.
+#   if dividend is even:
+#      goto 1
+#   if dividend is odd:
+#      goto 2
+# else:
+#   goto 2
+
+#if number is odd: #2
+#    divide by 3 and check for remainder == 0
+#    if remainder == 0
+#      save the dividend. This is the new number
+#      add three to the set of factors
+#
+#  divide by all primes less than the number
+#   if none divide evenly then you are done.
+# recusive?
+#
+#
+#
+#
+#
+#
+#
 
 
 
 
 
 
-def find_prime_factors(n): # int :=> [int]
 
-  pass
+
+
 
 
