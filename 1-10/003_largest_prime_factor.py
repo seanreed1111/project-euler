6dc02f17c874b6ca3e892bdd960b1abd3ddf971c
seanreed1111 <seanreed1111@gmail.com>
2019-05-18 20:42:56 -0400

    solves 003

--- 1-10/003_largest_prime_factor.py
+++ 1-10/003_largest_prime_factor.py
@@ -4,19 +4,34 @@ The prime factors of 13195 are 5, 7, 13 and 29.
 What is the largest prime factor of the number 600851475143?
 '''
 from math import sqrt
-#target = 600851475143
-target = 98
-# factors = find_prime_factors(target)
+from collections import Counter
+# def prime(n): # int :=> bool
+#   if n == 2: return True
+#   for i in range(2, int(sqrt(n)) + 1):
+#     if n % i == 0: return False
+#   return True
 
-# answer = max(factors)
+def find_factors(n): # int :=> [int]
+  factors = Counter({2:0})
+  #check even numbers
+  while (n % 2 == 0):
+    factors.update([2])
+    n = n // 2
+
+  for i in range(3,int(sqrt(n))+1,2):
+    while (n % i == 0):
+      factors.update([i])
+      n = n // i
+
+  return factors
+
+if __name__ == '__main__':
+
+#  target = 256
+#  target = 1280
+#  target = 3604957440
+  target = 600851475143
+  factors = find_factors(target)
+  print(max(factors))
 
 
-def prime(n): # int :=> bool
-  if n == 2: return True
-  for i in range(2, int(sqrt(n)) + 1):
-    if n % i == 0: return False
-  return True
-
-def find_prime_factors(n): # int :=> [int]
-
-  pass
--- lib/find_prime_factors.py
+++ lib/find_prime_factors.py
@@ -1,52 +1,12 @@
-numbers = [6,17,18,104]
+from math import sqrt
 def prime(n): # int :=> bool
   if n == 2: return True
   for i in range(2, int(sqrt(n)) + 1):
     if n % i == 0: return False
   return True
 
-#Question:
-#for any number n, how do you find the set of factors of n?
-#make a dict of prime numbers. Key and value
-# key is the number, value is the exponent, ie how many times the number divides evenly, e.g. prime factors of 40 = {2:3, 5:1}
-#make a set of prime numbers and check membership in the set.
-
-# if number is even: #1
-#   divide by two,
-#   increment two as a factor
-#   save the dividend. This is the new number
-#   if dividend is 1 then you are DONE.
-#   if dividend is even:
-#      goto 1
-#   if dividend is odd:
-#      goto 2
-# else:
-#   goto 2
-
-#if number is odd: #2
-#    divide by 3 and check for remainder == 0
-#    if remainder == 0
-#      save the dividend. This is the new number
-#      add three to the set of factors
-#
-#  divide by all primes less than the number
-#   if none divide evenly then you are done.
-# recusive?
-#
-#
-#
-#
-#
-#
-#
-
-
-
-
-
-
-
-
-
-
+if __name__ == '__main__':
+  numbers = [6,17,18,104]
+  for i in numbers:
+    print(i, prime(i))
 
