d35fa2c9659b15159574e4acb1f98da2cf31f5eb
seanreed1111 <seanreed1111@gmail.com>
2019-05-20 21:52:59 -0400

    adds stopiteration exception to break out of final loop in 009_special_pythagorean_triplet

--- 1-10/009_special_pythagorean_triplet.py
+++ 1-10/009_special_pythagorean_triplet.py
@@ -10,11 +10,14 @@ Find the product abc.
 '''
 from math import sqrt
 target_sum = 1000
-for c in range(target_sum):
-  for b in range(target_sum - c,1,-1):
-    for a in range(target_sum - c - b,1,-1):
-      if (a + b + c == 1000) and (a*b*c != 0) and (a**2 + b**2 - c**2)==0:
-        print("a,b,c",a,b,c)
-        print("a*b*c",a*b*c)
-        print("a^2 + b^2 - c^2",a**2 + b**2 - c**2)
-        raise StopIteration("Problem solution found.")
+try:
+  for c in range(target_sum):
+    for b in range(target_sum - c,1,-1):
+      for a in range(target_sum - c - b,1,-1):
+        if (a + b + c == 1000) and (a*b*c != 0) and (a**2 + b**2 - c**2)==0:
+          print("a,b,c",a,b,c)
+          print("a*b*c",a*b*c)
+          print("a^2 + b^2 - c^2 = ",a**2 + b**2 - c**2)
+          raise StopIteration("Problem solution found.")
+except StopIteration as e:
+  print(e)
